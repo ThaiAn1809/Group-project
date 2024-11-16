9a1067e6e34c58ff091de71e87192b0e153234ae
ThaiAn1809 <buithaian321@gmail.com>
2024-11-16 20:50:24 +0700

    Enter basics info

--- main.py
+++ main.py
@@ -19,6 +19,12 @@ import numpy
 # ...
 
 if __name__ == "__main__":
-    # Code place:
-    
-    pass
+    # define connection and cursor:
+    conn = sqlite3.connect("/data/database.db")
+    cur = conn.cursor()
+
+    # adding names, password and email:
+    username = input("Enter your username: ")
+    email = input("Enter your password: ")
+    password = input("Enter your password: ")
+    cur.execute(f"INSERT INTO users(username, email, password) VALUES ({username}, {email}, {password}) ")
