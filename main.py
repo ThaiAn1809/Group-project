ace1eb6a6764d6978b82cf71bcb5f100645d95da
ThaiAn1809 <buithaian321@gmail.com>
2024-11-16 20:20:54 +0700

    base skeleton for coding

---
+++ main.py
@@ -0,0 +1,24 @@
+# Chating place:
+
+# ...
+
+# Importing place:
+
+import math
+import random
+import sqlite3
+import numpy
+
+
+# Defining class place:
+
+# ... 
+
+# Defining function place:
+
+# ...
+
+if __name__ == "__main__":
+    # Code place:
+    
+    pass
