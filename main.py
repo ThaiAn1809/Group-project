d344d69fb91eb81932f857dd5fb748f038ceae80
ThaiAn1809 <buithaian321@gmail.com>
2024-11-16 21:30:00 +0700

    input for the username, email , password for the users table in database.db

--- main.py
+++ main.py
@@ -20,11 +20,40 @@ import numpy
 
 if __name__ == "__main__":
     # define connection and cursor:
-    conn = sqlite3.connect("/data/database.db")
+    conn = sqlite3.connect("database.db")
     cur = conn.cursor()
 
     # adding names, password and email:
     username = input("Enter your username: ")
-    email = input("Enter your password: ")
+    email = input("Enter your email: ")
     password = input("Enter your password: ")
-    cur.execute(f"INSERT INTO users(username, email, password) VALUES ({username}, {email}, {password}) ")
+    
+    # email and password list
+    email_list = []
+    password_list = []
+    
+    #fetch the email and password from the users table
+        
+    cur.execute("SELECT email, password FROM users")
+    rows = cur.fetchall()
+    
+    for row in rows:
+        email_data, password_data = row
+        email_list.append(email_data)
+        password_list.append(password_data)
+        
+    
+    # insert values for username, email and password:
+    if email not in email_list:
+        if password not in password_list:
+            cur.execute(f"""INSERT INTO users(username, email, password)
+                        VALUES (?, ?, ?)""", (username,email,password))
+        else:
+            print("INVAILD password")
+    else:
+        print("INVAILD email")
+        
+
+    # commit and close the connection:
+    conn.commit()
+    conn.close()
