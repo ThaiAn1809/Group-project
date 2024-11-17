3c7fd75cb1f009aa5fa048b828525c07278e8873
ThaiAn1809 <buithaian321@gmail.com>
2024-11-17 19:42:28 +0700

    commit and close

--- Thaian.py
+++ Thaian.py
@@ -8,5 +8,7 @@ username = input("Enter your username: ")
 email = input("Enter your email: ")
 password = input("Enter our password: ")
 
-cur.execute("INSERT INTO users VALUES(?,?,?)", (username,email,password))
+cur.execute("INSERT INTO users VALUES(username,email,password)", (username,email,password))
 
+conn.commit()
+cur.close()
