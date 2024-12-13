1ed1a4c5f3370174d9a893b28d0670d74c6f7b7d
ThaiAn1809 <buithaian321@gmail.com>
2024-12-13 21:55:26 +0700

    a

--- Everyone folder/Thai an folder/Thaiantest.py
+++ Everyone folder/Thai an folder/Thaiantest.py
@@ -1,5 +1,17 @@
-class console:
-    def log(message):
-        print(message)
-        
-console.log("bruh, javascript in python")
+class Calculator:
+    def add(self, *arg):
+        return sum(arg)
+
+
+
+
+calculator = Calculator()
+
+# Using the different overloaded add methods
+sum1 = calculator.add(2, 3)
+sum2 = calculator.add(2, 3, 4)
+sum3 = calculator.add(2, 3, 4, 5)
+
+print("Sum1:", sum1)
+print("Sum2:", sum2)
+print("Sum3:", sum3)
--- mainweb.js
+++ mainweb.js
@@ -1,5 +1,16 @@
-process.stdin.setEncoding('utf8');
+// process.stdin.setEncoding('utf8');
+// process.stdin.on('data', function(input) {
+//     const nums = input.trim().split('\n');
+//     let x = parseInt(nums[0]);
+//     let y = parseInt(nums[1]);
+//     console.log(x+y);
+//     console.log(x-y);
+// });
 
-process.stdin.on('data', function (input) {
-   console.log(input);
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(input) {
+   var mile = parseInt(input)
+   var km = mile * 1.60;
+   console.log(km);
 });
