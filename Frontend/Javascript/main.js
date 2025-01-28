0f1531e480bf3e3450ec74485a17d9cf2feae778
ThaiAn1809 <buithaian321@gmail.com>
2025-01-28 20:49:40 +0700

    make the server to work

--- Frontend/Javascript/main.js
+++ Frontend/Javascript/main.js
@@ -6,6 +6,9 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('Started server');
+})
 
 app.listen(port, () => {
     console.log(`Successfully started server on port ${port}.`);
