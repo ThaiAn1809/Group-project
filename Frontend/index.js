ba6d2e054aba0da33fe11644d5607b9beec64878
ThaiAn1809 <buithaian321@gmail.com>
2025-03-13 18:44:10 +0700

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -4,26 +4,26 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("homePage.ejs")
+  res.render("homePage.ejs");
 });
 
 app.get("/pricing", (req, res) => {
-    res.render("pricingPage.ejs")
-})
+  res.render("pricingPage.ejs");
+});
 
 app.get("/login", (req, res) => {
-    res.render("loginAndRegisterPage.ejs")
-})
+  res.render("loginAndRegisterPage.ejs");
+});
 
 app.get("/register", (req, res) => {
-    res.render("loginAndRegisterPage.ejs")
-})
+  res.render("loginAndRegisterPage.ejs");
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
